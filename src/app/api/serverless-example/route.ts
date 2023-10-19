2b338d1f70d895579656552876507d9cbb40db54
Dragos B <Dragos.Bucur@ibm.com>
2023-10-19 16:37:17 +0300

    Update

--- src/app/api/serverless-example/route.ts
+++ src/app/api/serverless-example/route.ts
@@ -1,14 +1,49 @@
 import { NextResponse } from 'next/server';
+import fs from 'fs'
+import path from 'path'
+import util from 'util'
+import { exec } from 'child_process'
+
+
+const execPromise = util.promisify(exec)
+
  
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const hasTitle = searchParams.has('title');
-  const title = hasTitle
-    ? searchParams.get('title')?.slice(0, 100)
-    : 'My default title';
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const hasTitle = searchParams.has('title');
+//   const title = hasTitle
+//     ? searchParams.get('title')?.slice(0, 100)
+//     : 'My default title';
  
+//   return NextResponse.json(
+//     { title },
+//     {
+//       status: 200,
+//     },
+//   );
+
+// }
+export async function GET() {
+  try {
+
+    await new Promise<void>((resolve, reject) => {
+      const child = exec('npx playwright test')
+  
+      // pipe to log files
+      child.on('close', async (code) => {
+        if (code != 0) {
+          reject(`playwright return code is non-zero: ${code}`)
+        } else {
+          resolve()
+        }
+      })
+    })
+  } catch(e) {
+    console.log('Avem o problema: ' + e) 
+  }
+
   return NextResponse.json(
-    { title },
+    { execution: 'finished' },
     {
       status: 200,
     },
