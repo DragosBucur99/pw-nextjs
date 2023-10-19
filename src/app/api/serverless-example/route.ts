7ec06231a98e2c388e7c9f1f57968ef150fd7916
Dragos B <Dragos.Bucur@ibm.com>
2023-10-19 16:10:20 +0300

    Update route

--- src/app/api/serverless-example/route.ts
+++ src/app/api/serverless-example/route.ts
@@ -1,7 +1,36 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { exec } from 'child_process'
 
-export function GET(request: NextRequest) {
+// export function GET(request: NextRequest) {
+//   return NextResponse.json(
+//     {
+//       body: request.body,
+//       path: request.nextUrl.pathname,
+//       query: request.nextUrl.search,
+//     },
+//     {
+//       status: 200,
+//     },
+//   );
+// }
+
+
+
+export async function GET(request: NextRequest) {
+  await new Promise<void>((resolve, reject) => {
+    const child = exec('npx playwright test')
+
+    child.on('close', async (code) => {
+      if (code != 0) {
+        reject(`playwright return code is non-zero: ${code}`)
+      } else {
+        console.log("Done")
+        resolve()
+      }
+    })
+  })
+
   return NextResponse.json(
     {
       body: request.body,
@@ -12,4 +41,4 @@ export function GET(request: NextRequest) {
       status: 200,
     },
   );
-}
+} 
