642f00aef70930404a6b2fcf9e885a0c681e559b
Dragos B <Dragos.Bucur@ibm.com>
2024-01-04 14:03:26 +0200

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,76 @@
+"use client";
+import { useState, useEffect } from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from "@nextui-org/react";
+
 export default function Footer() {
+  const [sectionIdToScroll, setSectionIdToScroll] = useState("");
+
+  const scrollToSection = (sectionId: string) => {
+    setSectionIdToScroll(sectionId);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const section = document.getElementById(sectionIdToScroll);
+
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    };
+
+    if (sectionIdToScroll) {
+      handleScroll();
+      setSectionIdToScroll("");
+    }
+  }, [sectionIdToScroll]);
+
+  const menuItems = [
+    {
+      name: "Home",
+      href: "hero-section",
+    },
+    {
+      name: "Playground",
+      href: "playground-section",
+    },
+    {
+      name: "Skills",
+      href: "skills-section",
+    },
+    {
+      name: "Contact",
+      href: "contact-section",
+    },
+  ];
+
   return (
-    <footer className="relative flex flex-wrap justify-between pt-5">
-      <ul className="w-[50%]">
-        <li>Logo</li>
-        <li>Social media</li>
-      </ul>
-      <ul className="w-[50%] text-end">
-        <li className="font-bold">Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+    <footer className="relative flex flex-wrap justify-between pt-10">
+      <div className="w-full flex justify-between">
+        <ul className="flex flex-col gap-1">
+          <h2 className="text-xl font-bold">Connect:</h2>
+          <Link
+            isExternal
+            showAnchorIcon
+            href="https://www.linkedin.com/in/dragos-bucur-b73966264/"
+            anchorIcon={<FaLinkedin />}
+          >
+            LinkedIn
+          </Link>
+        </ul>
+        <ul className="text-end flex flex-col gap-1">
+          {menuItems.map((item, index) => (
+            <li key={index} className="cursor-pointer">
+              <Link
+                className="text-white"
+                onClick={() => scrollToSection(item.href)}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <span className="text-xs mt-5 w-full text-center">
         Copyright {new Date().getFullYear()} © Dragos Portfolio
       </span>
