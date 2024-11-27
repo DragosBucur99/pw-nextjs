91768a2186ba2fddefe95895023bde0f5583bb95
Dragos B <bucur.dragos99@gmail.com>
2024-11-27 17:03:45 +0200

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import ToastProvider from "./toast.provider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Dragos Portfolio",
@@ -25,6 +26,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.className} dark`}>
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${"${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"}`}
+        />
+
+        <Script id="google-analytics">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${"${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"});
+  `}
+        </Script>
+      </head>
       <body className="bg-neutral-900">
         <Nav />
         <ToastProvider>
